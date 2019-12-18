9cc0eea3f7462f007bf3ea0a982bc8c024ec2d0b
DanPHP7 <danphp7@gmail.com>
2019-12-18 02:13:18 -0300

    global styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyles />
       <Routes />
     </Router>
   );
