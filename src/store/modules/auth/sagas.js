a5a97a521b7ee9f37b8d0fea696f130d1a7d24e8
DanPHP7 <danphp7@gmail.com>
2020-01-04 18:35:12 -0300

    feat: automatizando envio do token enquanto logado

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -21,6 +21,7 @@ export function* signIn({ payload }) {
       toast.error('Usuário não é provider');
       return;
     }
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -48,8 +49,18 @@ export function* signUp({ payload }) {
     yield put(signFailure());
   }
 }
+function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
 
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
